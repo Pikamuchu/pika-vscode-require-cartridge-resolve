d78326df23ed7b7657971a0cfa1db2958fa0abca
Antonio Marin Jimenez <antonio.marin.jimenez@gmail.com>
2018-12-27 05:28:42 +0100

    Added require navigation on isml files

--- src/RequireDefinitionProvider.ts
+++ src/RequireDefinitionProvider.ts
@@ -4,11 +4,12 @@ import * as fs from "fs";
 
 const REQUIRE_REGEX = /(require\s*\(\s*)(['"])\*\/cartridge(.*?[^\\])\2\s*\)/g;
 const REQUIRE_MATCH_PATH_POSITION = 4;
+const REQUIRE_TYPE = "require";
 const CARTRIDGE_DIR = "cartridge";
 
-export default class RequireDefinitionProvider
-  implements vscode.DefinitionProvider {
+export default class RequireDefinitionProvider implements vscode.DefinitionProvider {
   private _configuration: any = {};
+  private _lastFileItem: any = {};
 
   constructor(configuration) {
     this._configuration = Object.assign(this._configuration, configuration);
@@ -16,8 +17,7 @@ export default class RequireDefinitionProvider
 
   provideDefinition(
     document: vscode.TextDocument,
-    position: vscode.Position,
-    token: vscode.CancellationToken
+    position: vscode.Position
   ): vscode.ProviderResult<vscode.Definition> {
     let result = null;
     const line = document.lineAt(position.line);
@@ -27,27 +27,25 @@ export default class RequireDefinitionProvider
     return result;
   }
 
-  private async findRequireFilePosition(
-    lineText: string,
-    document: vscode.TextDocument,
-    position: vscode.Position
-  ) {
+  private async findRequireFilePosition(lineText: string, document: vscode.TextDocument, position: vscode.Position) {
     let result = null;
     try {
-      let fileItem = await this.findRequirePath(lineText, REQUIRE_REGEX);
-      if (fileItem) {
-        // Check path on current context
-        let filePath = await this.resolveFilePath(fileItem, document);
-        if (!filePath) {
-          // Trying to find file on workspace
-          filePath = await this.findFilePath(fileItem, document);
-        }
-        if (filePath) {
-          console.log('Resolved file path "' + filePath + '"');
-          result = new vscode.Location(
-            vscode.Uri.file(filePath),
-            new vscode.Range(0, 0, 0, 0)
-          );
+      result = this.getFileItemResultFromCache(lineText, document.fileName);
+      if (!result) {
+        let fileItem = await this.findRequirePath(lineText, REQUIRE_REGEX);
+        if (fileItem) {
+          // Check path on current context
+          fileItem.documentFileName = document.fileName;
+          let filePath = await this.resolveFilePath(fileItem);
+          if (!filePath) {
+            // Trying to find file on workspace
+            filePath = await this.findFilePath(fileItem);
+          }
+          if (filePath) {
+            console.log('Resolved file path "' + filePath + '"');
+            result = new vscode.Location(vscode.Uri.file(filePath), new vscode.Range(0, 0, 0, 0));
+            this.storeLastFileItem(fileItem, filePath, result);
+          }
         }
       }
     } catch (error) {
@@ -56,14 +54,16 @@ export default class RequireDefinitionProvider
     return result;
   }
 
-  private findRequirePath(text: string, expr: RegExp): Promise<any> {
+  private findRequirePath(lineText: string, expr: RegExp): Promise<any> {
     return new Promise((resolve, reject) => {
       let result = null;
 
-      let match = expr.exec(text);
+      let match = expr.exec(lineText);
       if (match && match.length >= REQUIRE_MATCH_PATH_POSITION) {
         result = {
-          path: match[REQUIRE_MATCH_PATH_POSITION - 1]
+          lineText: lineText,
+          path: match[REQUIRE_MATCH_PATH_POSITION - 1],
+          type: REQUIRE_TYPE
         };
       }
 
@@ -71,46 +71,73 @@ export default class RequireDefinitionProvider
     });
   }
 
-  private resolveFilePath(fileItem, document): Promise<any> {
+  private resolveFilePath(fileItem): Promise<any> {
     if (fileItem && fileItem.path) {
       return new Promise((resolve, reject) => {
-        const fileName = path.dirname(document.fileName);
-        const cartridgeIndex =
-          fileName.indexOf("\\") >= 0
-            ? CARTRIDGE_DIR + "\\"
-            : CARTRIDGE_DIR + "/";
-        let filePath = fileName.substring(0, fileName.indexOf(cartridgeIndex));
-        filePath += CARTRIDGE_DIR + fileItem.path + ".js";
-        // Check if filename exists
-        fs.stat(filePath, (error, stat) => {
-          let resolvedFilePath = null;
-          if (!error) {
-            resolvedFilePath = filePath;
-          }
-          return resolve(resolvedFilePath);
-        });
+        try {
+          // Create file path
+          const fileName = fileItem.documentFileName;
+          const initialPath = fileName.substring(0, fileName.indexOf(CARTRIDGE_DIR + path.sep));
+          const filePath = initialPath + CARTRIDGE_DIR + fileItem.path + this.resolveExtension(fileItem);
+          // Check if file path exists
+          fs.stat(filePath, (error, stat) => {
+            let resolvedFilePath = null;
+            if (!error) {
+              resolvedFilePath = filePath;
+            }
+            return resolve(resolvedFilePath);
+          });
+        } catch (error) {
+          console.log("resolveFilePath: " + error);
+          return resolve(null);
+        }
       });
     } else {
       return Promise.reject("resolveFilePath: File path is undefined");
     }
   }
 
-  private findFilePath(fileItem, document): Promise<any> {
+  private findFilePath(fileItem): Promise<any> {
     if (fileItem && fileItem.path) {
       return new Promise((resolve, reject) => {
-        const includePattern = "**/" + CARTRIDGE_DIR + "/**" + fileItem.path + ".js";
-        vscode.workspace.findFiles(includePattern)
-          .then(files => {
+        try {
+          const includePattern = "**/" + CARTRIDGE_DIR + "/**" + fileItem.path + this.resolveExtension(fileItem);
+          vscode.workspace.findFiles(includePattern, "**/node_modules/**").then(files => {
             let resolvedFilePath = null;
             if (files && files.length > 0) {
               resolvedFilePath = files[0].fsPath;
             }
             return resolve(resolvedFilePath);
           });
+        } catch (error) {
+          console.log("findFilePath: " + error);
+          return resolve(null);
+        }
       });
     } else {
       return Promise.reject("findFilePath: File path is undefined");
     }
+  }
+
+  private resolveExtension(fileItem) {
+    const documentFileName = fileItem.documentFileName;
+    const addExtension = fileItem.path.indexOf(".") <= 0;
+    return addExtension ? documentFileName.substr(documentFileName.lastIndexOf(".")) : "";
+  }
+
+  getFileItemResultFromCache(lineText, documentFileName) {
+    const lastFileItem = this._lastFileItem;
+    if (lastFileItem && lastFileItem.lineText === lineText && lastFileItem.documentFileName === documentFileName) {
+      console.log('From cache file path "' + lastFileItem.filePath + '"');
+      return lastFileItem.result;
+    }
+    return null;
+  }
+
+  private storeLastFileItem(fileItem, filePath, result) {
+    fileItem.filePath = filePath;
+    fileItem.result = result;
+    this._lastFileItem = fileItem;
   }
 
   dispose() {
--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,15 @@
-import * as vscode from 'vscode';
-import RequireDefinitionProvider from './RequireDefinitionProvider';
+import * as vscode from "vscode";
+import RequireDefinitionProvider from "./RequireDefinitionProvider";
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('cartridge resolve extension activing');
-    const configuration = vscode.workspace.getConfiguration('require.cartridge.resolve');
-    const definitionProvider = new RequireDefinitionProvider(configuration);
-    const registerDefinitionProvider = vscode.languages.registerDefinitionProvider({ scheme: 'file', pattern: '**/*.{js,ts}' }, definitionProvider);
-    context.subscriptions.push(registerDefinitionProvider);
+  console.log("cartridge resolve extension activing");
+  // Getting configuration
+  const configuration = vscode.workspace.getConfiguration("require.cartridge.resolve");
+  // Registering provider
+  const selector = { scheme: "file", pattern: "**/*.{" + configuration.filetypes + "}" };
+  const provider = new RequireDefinitionProvider(configuration);
+  const registerDefinitionProvider = vscode.languages.registerDefinitionProvider(selector, provider);
+  context.subscriptions.push(registerDefinitionProvider);
 }
 
 export function deactivate() {}
