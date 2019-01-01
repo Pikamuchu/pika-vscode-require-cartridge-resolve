f95a2c9d3f5a6145153111371949ea3519d70145
Antonio Marin Jimenez <antonio.marin.jimenez@gmail.com>
2019-01-01 07:02:44 +0100

    Adding jsdoc

--- src/RequireDefinitionProvider.ts
+++ src/RequireDefinitionProvider.ts
@@ -8,10 +8,14 @@ const requireDefinitionConfig: DefinitionConfig = {
   identifyType: "require",
 };
 
+/**
+ * Definition Provider for scripts in "require" statements.
+ *  @example
+ * var collections = require('unicodeEscape('*')/cartridge/scripts/util/collections');
+ * 
+ */
 export default class RequireDefinitionProvider extends BaseDefinitionProvider implements vscode.DefinitionProvider {
-
   constructor(extensionConfig = {}, definitionConfig = requireDefinitionConfig) {
     super(extensionConfig, definitionConfig);
   }
-
 }
