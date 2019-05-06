9adbdc9b0e664f7c228fcd639b00bc3a2de7c833
Antonio Marin Jimenez <antonio.marin.jimenez@gmail.com>
2019-05-06 22:06:14 +0200

    Adding label cleaning regex

--- src/DefaultDefinitionProvider.ts
+++ src/DefaultDefinitionProvider.ts
@@ -12,7 +12,7 @@ const RESOLVED_TYPE_COMPLETION = "COMPLETION";
 
 const MAX_EXTRACT_REGEX_LOOP = 10;
 
-const HOVER_MESSAGE_HEADER = "``` js\n"
+const HOVER_MESSAGE_HEADER = "``` js\n";
 const PIKA_EMOJI_FOUND = "ϞϞ(๑⚈‿‿⚈๑)∩ - ";
 const PIKA_EMOJI_NOT_FOUND = "ϞϞ(๑⊙__☉๑)∩ - no ";
 
@@ -36,7 +36,8 @@ const defaultDefinitionConfig: DefinitionConfig = {
     /\s*:\s*[a-zA-Z]+\s*/g,
     /\s*\|\s*[a-zA-Z]+/g,
     /\s*\<[a-zA-Z]+\>\s*/g,
-    /\s+$/
+    /\s+$/,
+    /readonly\s+/
   ]
 };
 
