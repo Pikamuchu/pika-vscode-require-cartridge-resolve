3f9a8b7186489005923bcb9821367abc26dcefdb
Antonio Marin Jimenez <antonio.marin.jimenez@gmail.com>
2020-08-27 08:52:49 +0200

    Adding test TODO

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,27 +1,20 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as myExtension from '../src/extension';
 
 suite("Extension Tests", () => {
-    test("My extension should be available", () => {
+    test("Extension should be available", () => {
         assert.ok(myExtension, "Extension not available!");
     });
-    test('open order controller file', async () => {
-      const exampleWorkspace = '../examples/workspace';
-      const exampleWorkspaceUri = vscode.Uri.file(exampleWorkspace);
-      const orderControllerFile = '../examples/workspace/project2/cartridges/extension/cartridge/controllers/Order.js';
-  
-      await vscode.commands.executeCommand('vscode.openFolder', exampleWorkspaceUri);
-      const doc = await vscode.workspace.openTextDocument(orderControllerFile);
-      await vscode.window.showTextDocument(doc);
+
+    test('Example workspace test files should be resolved', async () => {
+      const testFiles = await vscode.workspace.findFiles("**/cartridge/**/*.js", "**/node_modules/**");
+      if (testFiles && testFiles.length > 0) {
+        testFiles.forEach(async (testFile) => {
+          const testDocument = await vscode.workspace.openTextDocument(testFile);
+          await vscode.window.showTextDocument(testDocument);
+          // TODO: test TEST: marks
+        });
+      }
     });
 });
